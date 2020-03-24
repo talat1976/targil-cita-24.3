cd1df48ef1ad065855e835e754538e44335a4211
talat1976 <62280134+talat1976@users.noreply.github.com>
2020-03-24 21:54:18 +0200

    Add files via upload

---
+++ matala1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // number to arr lenght
+            Console.WriteLine("enter your length the arr : ");
+            int longer = Convert.ToInt32(Console.ReadLine());
+            //number to *
+            Console.WriteLine("enter your many to zoom : ");
+            //convert sting to int
+            int zoom = Convert.ToInt32(Console.ReadLine());
+            //make a arr
+            int[] arr = new int[longer];
+            Console.WriteLine($"number of lengh :{longer} , number of zoom : {zoom}");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i * zoom;
+                Console.Write(arr[i] + ",");
+            }
+            Console.ReadKey();
+        }
+    }
+}
